7a795ecd77936a3ee884d6b8ff4d7ccd783ab885
LasseCHels <lass4279@edu.campusvejle.dk>
2018-02-12 23:36:23 +0100

    Styled party button in accordance with party guidelines. Added a reload button to prevent browser crashing

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     const box = $('#container');
     
     const btn = $('#partyTime');
+    const resetBtn = $('#reset');
     
     const className = ".confetti";
     
@@ -42,8 +43,8 @@ $(document).ready(function() {
     // Array containing the confetti background colors
     const colorArr = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D', '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC', '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399', '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933', '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
     
-    // Bool to check whether cannon has fired
-    let hasFired = false;
+    // Bool to check whether cannon is ready to fire
+    let readyToFire = true;
     
     // Creates specified amount of confetti
     function CreateConfetti(amount) {
@@ -57,7 +58,7 @@ $(document).ready(function() {
             // Generating the confetti background color
             const color = colorArr[Rand(0, colorArr.length)];
             
-            // Creating the confetti as the specified type
+            // Creating the confetti as the specified element type
             const c = $(elementType);
             
             // Setting height and width
@@ -83,15 +84,10 @@ $(document).ready(function() {
     // Launches all confetti into the air by changing their left and top positions with a transition effect
     function Fire() {
         
-        // If confetti has already been launched, reset their position and launch again
-        if (hasFired) {
+        // Check to see if cannon is ready to fire
+        if (readyToFire) {
             
-            ResetConfetti();
-            hasFired = false;
-            
-        } else {
-            
-            hasFired = true;
+            readyToFire = false;
             
             // Loops through all confetti inside the container
             box.children(className).each(function() {
@@ -147,7 +143,6 @@ $(document).ready(function() {
             
         }
         
-        
     }
     
     // Resets the position of each confetti
@@ -164,6 +159,8 @@ $(document).ready(function() {
             });
             
         });
+        
+        readyToFire = true;
     }
     
     // Returns random whole number
@@ -182,8 +179,8 @@ $(document).ready(function() {
     
     CreateConfetti(confettiAmount);
     
-    btn.click(function() {
-        Fire();
-    });
+    btn.click(Fire);
+    
+    resetBtn.click(ResetConfetti);
     
 });
