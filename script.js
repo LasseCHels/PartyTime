e65722c15b1b9004664656df69f070b0a94bd52c
LasseCHels <lass4279@edu.campusvejle.dk>
2018-02-12 22:45:13 +0100

    Fixed bug occuring with multiple rapid button presses

--- script.js
+++ script.js
@@ -42,6 +42,9 @@ $(document).ready(function() {
     // Array containing the confetti background colors
     const colorArr = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D', '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC', '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399', '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933', '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
     
+    // Bool to check whether cannon has fired
+    let hasFired = false;
+    
     // Creates specified amount of confetti
     function CreateConfetti(amount) {
         
@@ -81,12 +84,14 @@ $(document).ready(function() {
     function Fire() {
         
         // If confetti has already been launched, reset their position and launch again
-        if ($(className).css("top") != startPosTop + "px") {
+        if (hasFired) {
             
             ResetConfetti();
-            Fire();
+            hasFired = false;
             
         } else {
+            
+            hasFired = true;
             
             // Loops through all confetti inside the container
             box.children(className).each(function() {
